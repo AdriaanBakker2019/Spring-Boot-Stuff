b43f4d13d5f52616abe034f7bdaf31ac93f3cf0f
Adriaan Bakker <adriaan.bakker@lunatech.nl>
2019-07-22 09:38:25 +0200

    changed name add_dog.html and DogsFrontEndController, entry point /adddog

---
+++ JPADemo002/src/main/java/dogapps/web/DogsFrontEndController.java
@@ -0,0 +1,36 @@
+package dogapps.web;
+
+import dogapps.model.DogDto;
+import dogapps.repo.Dog;
+import dogapps.service.DogsService;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+
+@Controller
+public class DogsFrontEndController {
+    @Autowired
+    DogsService service;
+
+
+    @GetMapping("/adddog")
+    public String greetingForm(Model model) {
+        model.addAttribute("dog", new Dog());
+        return "add_dog";
+    }
+
+    @PostMapping("/adddog")
+    public String greetingSubmit(@ModelAttribute Dog dog) {
+        DogDto dogdto = new DogDto();
+        dogdto.setAge(dog.getAge());
+        dogdto.setId(dog.getId());
+        dogdto.setName(dog.getName());
+        service.add(dogdto);
+        return "result";
+    }
+
+}
